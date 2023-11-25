e21f33ad28b49ca38aba72f91b95f9a26d9b4d92
Angel Tirado <107122795+AngelTirado8@users.noreply.github.com>
2023-11-25 11:24:45 -0300

    creacion de otrs endpoint

--- back/src/models/user.js
+++ back/src/models/user.js
@@ -1,20 +1,22 @@
-const mongoose = require ('mongoose')
-
+const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    id:{
-        type:String,
-        required:true
-    }
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  id: {
+    type: String,
+    required: false
+  }
 })
 
 module.exports = mongoose.model('User', userSchema)
-
