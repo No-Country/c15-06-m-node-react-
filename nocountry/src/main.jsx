010a1f8067fc004fa420e6432a01b746f35c3786
Bogaralex135 <bogarpacho@gmail.com>
2023-11-25 21:49:26 -0600

    Router Provider and redux provider

--- nocountry/src/main.jsx
+++ nocountry/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router/Routes'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
 )
