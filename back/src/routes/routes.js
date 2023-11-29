ee80de877ded48bef9082907598c81ec4e0b2de6
Angel Tirado <107122795+AngelTirado8@users.noreply.github.com>
2023-11-29 15:40:54 -0300

    filtros combinados

--- back/src/routes/routes.js
+++ back/src/routes/routes.js
@@ -1,9 +1,9 @@
 const express = require('express')
-// const Router = require('express')
 const { createUser } = require('../controllers/createUser')
 const { updateUser } = require('../controllers/updateUser.js')
 const { getUserById } = require('../controllers/getUserById.js')
 const { getAllProducts } = require('../controllers/getAllProducts.js')
+const { getProductsByCategory } = require('../controllers/getProductByCategory.js')
 const { getAllUsers } = require('../controllers/getAllUsers.js')
 const { getProductById } = require('../controllers/getProductById.js')
 const { createProduct } = require('../controllers/createProduct')
@@ -20,6 +20,7 @@ router.put('/users/:id', updateUser)
 // rutas productos
 router.post('/product', createProduct)
 router.get('/product', getAllProducts)
+router.get('/product/category/:categoria', getProductsByCategory)
 router.get('/product/:id', getProductById)
 router.put('/product/:id', updateProduct)
 // session
