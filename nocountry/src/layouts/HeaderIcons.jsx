1bd93411a2fec81ece47acc5c6a1b7dacaefb0b3
natitejeda <natybrenda26@gmail.com>
2023-12-18 23:12:03 -0300

    Se modifican colores de iconos  en header

--- nocountry/src/layouts/HeaderIcons.jsx
+++ nocountry/src/layouts/HeaderIcons.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 export function HeaderIcons({ ...props }) {
   return (
     <div className='flex gap-4' {...props}>
-      <HeartIcon fill='#000' width={40} height={40} />
-      <UserIcon fill='#000' width={40} height={40} />
+      <HeartIcon fill='#152D2F' width={40} height={40} />
+      <UserIcon fill='#152D2F' width={40} height={40} />
       <Link to='/cart'>
-        <CartIcon fill='#000' width={40} height={40} />
+        <CartIcon fill='#152D2F' width={40} height={40} />
       </Link>
     </div>
   )
