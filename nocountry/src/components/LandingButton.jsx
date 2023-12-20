bb0156cdecb5f78a8629c7227fbfcecf9a6741cc
natitejeda <natybrenda26@gmail.com>
2023-12-19 23:41:06 -0300

    Se agrega botón en landing page

---
+++ nocountry/src/components/LandingButton.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export function LandingBoton() {
+    return (
+        <div className="mx-40 -mt-40">
+            <button className="w-[360px] h-20 text-white bg-palette-quaternary font-semibold text-2xl px-8 py-3 rounded-lg border border-palette-quaternary hover:bg-white hover:text-palette-quaternary font-rajdhani">
+                <Link to={``}>Explorar ahora</Link>
+            </button>
+        </div>
+    )
+}
